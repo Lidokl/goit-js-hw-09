7bc0d347e560cbef73ff3d04f36131f2e501d2e0
Lidija Levchenko <levchenkola@ukr.net>
2022-01-03 13:00:40 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,28 +3,70 @@ import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
 
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.2.min.css';
+
 
 
 const refs = {
-    input: document.querySelector('.datetime-picker'),
-    button: document.querySelector('.data-start') ,
-    days: document.querySelector('.data-days'),
-    hours: document.querySelector('.data-hours'),
-    minutes: document.querySelector('.data-minutes'),
-    seconds: document.querySelector('.data-seconds')
+    input: document.querySelector('#datetime-picker'),
+    button: document.querySelector('[data-start]') ,
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]')
 
 };
 
+refs.button.disabled = true;
+// refs.input.addEventListener('input', setDate);
+let timerId = null;
+let choseDate = 0;
+let timeLeft = 0;
+// function setDate(e) {
+//     date = new Date(e.target.value).getTime();
+// }
 // refs.button.addEventListener();
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+      // console.log(selectedDates[0]);
+  
+    const selectedDate = selectedDates[0].getTime();
+    const currentDate = this.config.defaultDate.getTime();
+    if (currentDate > selectedDate) {
+      return Notiflix.Report.failure('Please choose a date in the future');
+    }
+
+    refs.button.disabled = false;
+    }, }
+
+
+    flatpickr(refs.input, options);
+
+    refs.button.addEventListener('click', startClock);
+  
+  function startClock() {
+    refs.button.removeEventListener('click', startClock);
+    timerId = setInterval(() => {
+      let nowDate = Date.now();
+      timeLeft = choseDate - nowDate;
+       updateClockFase(convertMs(timeLeft));
+      console.log(timeLeft);
+      if (timeLeft < time) {
+        clearInterval(timerId);
+      }
+    }, 1000);
+  }
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');}
+
+
 function convertMs(ms) {
     // Number of milliseconds per unit of time
     const second = 1000;
@@ -47,4 +89,9 @@ function convertMs(ms) {
   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+  
+
+  // function welcomToNotiflix() {
+  //   console.log('Please choose a date in the future')
+  // }
   
