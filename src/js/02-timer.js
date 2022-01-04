d3549910e9ce16ad38a874f805fdcca95c65cd74
Lidija Levchenko <levchenkola@ukr.net>
2022-01-04 17:07:12 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -18,15 +18,27 @@ const refs = {
 
 };
 
-refs.button.disabled = true;
+// refs.button.disabled = true;
 // refs.input.addEventListener('input', setDate);
 let timerId = null;
-let choseDate = 0;
-let timeLeft = 0;
+let choseDate = null;
+
 // function setDate(e) {
 //     date = new Date(e.target.value).getTime();
 // }
 // refs.button.addEventListener();
+function checkDate(dt) {
+  refs.button.disabled = true;
+
+  const now = new Date();
+  if (dt < now) {
+      Notiflix.Report.failure("Please choose a date in the future");
+      return;
+  }
+  refs.button.disabled = false;
+  choseDate = dt.getTime();
+}
+
 
 const options = {
     enableTime: true,
@@ -34,15 +46,16 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
+      checkDate(selectedDates[0]);
       // console.log(selectedDates[0]);
   
-    const selectedDate = selectedDates[0].getTime();
-    const currentDate = this.config.defaultDate.getTime();
-    if (currentDate > selectedDate) {
-      return Notiflix.Report.failure('Please choose a date in the future');
-    }
+    // const selectedDate = selectedDates[0].getTime();
+    // const currentDate = this.config.defaultDate.getTime();
+    // if (currentDate > selectedDate) {
+    //   return Notiflix.Report.failure('Please choose a date in the future');
+    // }
 
-    refs.button.disabled = false;
+    // refs.button.disabled = false;
     }, }
 
 
@@ -53,22 +66,30 @@ const options = {
   function startClock() {
     refs.button.removeEventListener('click', startClock);
     timerId = setInterval(() => {
-      let nowDate = Date.now();
-      timeLeft = choseDate - nowDate;
-       updateClockFase(convertMs(timeLeft));
+      let nowDate = Date.now().getTime();
+      time = choseDate - nowDate;
+       updateClockFase(convertMs(time));
       // console.log(timeLeft);
-      if (timeLeft < time) {
+      if (time < 0) {
         clearInterval(timerId);
+        return;
+      }
+      const res = convertMs(t);
+
+      days.innerHTML = addLeadingZero(res.days);
+      hours.innerHTML = addLeadingZero(res.hours);
+      minutes.innerHTML = addLeadingZero(res.minutes);
+      seconds.innerHTML = addLeadingZero(res.seconds);
+      // console.log(res);
+  }, 1000);
+  Notiflix.Notify.success();
+};
+
+  function addLeadingZero(value) {
+        return String(value).padStart(2, '0');
       }
 
-      function addLeadingZero(value) {
-        return String(value).padStart(2, '0');}
-
-   
   
-
-
-
 
 function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -87,17 +108,10 @@ function convertMs(ms) {
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   
     return { days, hours, minutes, seconds };
-  }
-  
+  } 
+
   // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
   // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
   // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-  function updateClockFase({ days, hours, minutes, seconds }) {
-    refs.days.innerHTML = `${days}`;
-    refs.hours.innerHTML = `${hours}`;
-    refs.minutes.innerHTML = `${minutes}`;
-    refs.seconds.innerHTML = `${seconds}`;
-  
-  Notiflix.Notify.success()
-  }})}
+ 
