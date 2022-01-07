80f66ea1fedde861a1ec993d7dfe3e473d9b6c91
Lidija Levchenko <levchenkola@ukr.net>
2022-01-07 13:13:09 +0200

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,42 +5,38 @@ import 'notiflix/dist/notiflix-3.2.2.min.css';
 const refs = {
   form: document.querySelector('form'),
   inputs: document.querySelectorAll('input'),
-}
-refs.form.addEventListener('submit', onBtnSubmit);
-
-function  onBtnSubmit(evt) {
-  evt.preventDefault();
-
-
-  createPromise(position, delay)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-
-}
-
-
-
-
-
-
-
+};
+let inputsValue = [];
+let timerId = null;
+let counter = 0;
+// =============================================================================
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    shouldResolve({position, delay})
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    reject({position, delay})
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
-let today = new Date();
-let newYear = new Date(2022, 0, 1, 0, 0, 1);
+// =============================================================================
+refs.form.addEventListener('submit', startIvents);
+// =======================================
+function startIvents(e) {
+  e.preventDefault();
+  refs.inputs.forEach(getInputsValue);
+  let [delay, step, amount] = inputsValue;
 
-if (today.valueOf() === newYear.valueOf()) {
-  console.log('Happy New Year!!!');
+  timerId = setInterval(() => {
+    counter += 1;
+    if (amount <= counter) {
+      clearInterval(timerId);
+    }
+    createPromise(counter, delay);
+    delay += step;
+    console.log(counter);
+  }, delay);
+}
+// ===================================================
+function getInputsValue(input) {
+  return inputsValue.push(Number(input.value));
 }
